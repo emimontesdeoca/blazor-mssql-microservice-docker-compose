05d430e5a04bd6129cbc9cfa46f6bdd48284eebd
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2024-12-11 16:47:38 +0000

    wip

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthorization();
 builder.Services.AddHealthChecks();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource
+        .AddService(serviceName: builder.Environment.ApplicationName))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter());
 
 var app = builder.Build();
 
@@ -16,6 +25,8 @@ app.MapGet("/test", () =>
 {
     return Results.Ok($"{Guid.NewGuid()}");
 });
+
+app.MapPrometheusScrapingEndpoint();
 
 app.MapHealthChecks("/health");
 
