9c13f4e481fd8b5d8f018b6523a1fe9e83e8ddf1
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2024-12-11 17:04:15 +0000

    wip

--- Api/Program.cs
+++ Api/Program.cs
@@ -10,19 +10,6 @@ builder.Services.AddAuthorization();
 builder.Services.AddHealthChecks();
 
 builder.Services.AddOpenTelemetry()
-    .WithMetrics(builder =>
-    {
-        builder.AddPrometheusExporter();
-
-        builder.AddMeter("Microsoft.AspNetCore.Hosting",
-                         "Microsoft.AspNetCore.Server.Kestrel");
-        builder.AddView("http.server.request.duration",
-            new ExplicitBucketHistogramConfiguration
-            {
-                Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
-                       0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
-            });
-    })
     .WithTracing(tracing => tracing
         .AddAspNetCoreInstrumentation()
         .AddConsoleExporter());
