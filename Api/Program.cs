f5a6bed5b9b89b4c81b7ceb755bfb07b0b984169
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2024-12-12 08:57:21 +0000

    wip

--- Api/Program.cs
+++ Api/Program.cs
@@ -9,16 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthorization();
 builder.Services.AddHealthChecks();
 
-builder.Logging.ClearProviders();
-builder.Logging.AddOpenTelemetry(x =>
-{
-    x.AddOtlpExporter(a =>
-    {
-        a.Endpoint = new Uri("http://seq:5341/ingest/otlp/v1/logs");
-        a.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
-        a.Headers = "X-Seq-ApiKey=12345678901234567890";
-    });
-});
+//builder.Logging.ClearProviders();
+//builder.Logging.AddOpenTelemetry(x =>
+//{
+//    x.AddOtlpExporter(a =>
+//    {
+//        a.Endpoint = new Uri("http://seq:5341/ingest/otlp/v1/logs");
+//        a.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+//        a.Headers = "X-Seq-ApiKey=12345678901234567890";
+//    });
+//});
 
 var app = builder.Build();
 
