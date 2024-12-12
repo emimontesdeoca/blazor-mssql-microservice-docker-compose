eb95e205de96ef71fb0d03c8f13e0a0747d66024
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2024-12-12 08:50:59 +0000

    wip

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Http.HttpResults;
+using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
@@ -9,12 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthorization();
 builder.Services.AddHealthChecks();
 
-//builder.Logging.ClearProviders();
-//builder.Logging.AddOpenTelemetry(x => {
-//x.addotlpExporter
-//});
-
-
+builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(x =>
+{
+    x.AddOtlpExporter(a =>
+    {
+        a.Endpoint = new Uri("http://seq:5341/ingest/otlp/v1/logs");
+        a.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+        a.Headers = "X-Seq-ApiKey=12345678901234567890";
+    });
+});
 
 var app = builder.Build();
 
