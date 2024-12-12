6d949e9e30cddea82e9c7bbad609efda7fda7ff0
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2024-12-12 10:17:35 +0000

    wip

--- Api/Program.cs
+++ Api/Program.cs
@@ -23,12 +23,52 @@ app.MapGet("/test", () =>
     return Results.Ok($"{Guid.NewGuid()}");
 });
 
+
 app.MapHealthChecks("/health");
 
 app.Run();
 
 
 static IHostApplicationBuilder ConfigureOpenTelemetry(IHostApplicationBuilder builder)
+{
+    builder.Services.AddOpenTelemetry()
+        .ConfigureResource(c => c.AddService("MyApp"))
+        .WithMetrics(metrics =>
+        {
+            metrics.AddHttpClientInstrumentation()
+                   .AddRuntimeInstrumentation()
+                   .AddAspNetCoreInstrumentation();
+        })
+        .WithTracing(tracing =>
+        {
+            tracing.AddHttpClientInstrumentation()
+                   .AddAspNetCoreInstrumentation();
+        });
+
+    // Use the OTLP exporter if the endpoint is configured.
+    var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+    if (useOtlpExporter)
+    {
+        builder.Services
+            .AddOpenTelemetry()
+            .UseOtlpExporter();
+    }
+    else
+    {
+        Console.WriteLine("OTLP Exporter not configured");
+    }
+
+    builder.Logging.AddOpenTelemetry(logging =>
+    {
+        logging.IncludeFormattedMessage = true;
+        logging.IncludeScopes = true;
+        logging.AddOtlpExporter();
+    });
+
+    return builder;
+}
+
+static IHostApplicationBuilder ConfigureOpenTelemetry2(IHostApplicationBuilder builder)
 {
     var endpoint = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT")!;
 
